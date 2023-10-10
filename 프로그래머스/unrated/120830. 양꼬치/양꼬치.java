d8e60a76f9a6a6dda808c1c3be362105f05ebb8b
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2023-10-10 18:35:37 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int service = n/10;
+        int answer = n*12000 + k*2000 -service*2000;
+        
+        return answer;
+    }
+}
