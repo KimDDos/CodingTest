c51a7c47cb1a6a7fe5faff2ba173d5c107781273
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2023-10-27 14:47:49 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,21 +1,6 @@
 class Solution {
     public int solution(int left, int right) {
-        int mea = 0; // 약수 개수 구하기
-        int odd = 0;
-        int even = 0;
-        for(int i=left; i<=right; i++){
-            for(int k=1; k<=i; k++){
-                if(i%k==0) mea++;
-            }   
-            if(mea%2==0){
-                even += i;
-            } else {
-                odd += i;
-            }
-            mea=0;
-        }
-        
-        int answer = even - odd;
+        int answer = 0;
         return answer;
     }
 }
