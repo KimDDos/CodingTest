17b0dee73d2fb30ab1f5623354284ab97aa27a26
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2023-12-20 00:52:44 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 1.62 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] strArr = my_string.split("");
+		String tmp = strArr[num1];
+		strArr[num1] = strArr[num2];
+		strArr[num2] = tmp; 
+		String answer = "";
+		
+		for(String str : strArr) {
+			answer += str;
+		}
+        return answer;
+    }
+}
