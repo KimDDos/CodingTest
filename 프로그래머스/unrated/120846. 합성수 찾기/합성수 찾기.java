1102b4f23b11578ae1d6cfb2fab595ac93ecb78c
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2023-12-31 15:30:31 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=4; i<=n; i++) {
+			int count = 0;
+			for(int k=1; k<=i; k++) {
+				if(i % k == 0) {
+					count++;
+					if(count >= 3) {
+						answer++;
+						break;
+					}
+				}
+			}
+		}
+        return answer;
+    }
+}
