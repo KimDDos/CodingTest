76b63573e6ef2442f88e04e6ea07d28dd2474b75
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2026-02-28 01:30:15 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,23 +1,25 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = numCheck(a, b, c);
+        int answer = 0;
+        int match = 0;
+
+        if(a == b){
+          match++;
+        }
+        if(a == c){
+          match++;
+        }
+        if(b == c){
+          match++;
+        }
+
+        if(match == 0){
+          answer = a + b + c;
+        }else if(match == 1){
+          answer = (a+b+c) * (a*a + b*b + c*c);
+        }else if(match == 3){
+          answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        }
         return answer;
     }
-    
-    public static int numCheck(int a, int b, int c) {
-		int answer = 0;
-		int cal1 = a + b + c;
-		int cal2 = ((int)Math.pow(a, 2) + (int)Math.pow(b, 2) + (int)Math.pow(c, 2));
-		int cal3 = ((int)Math.pow(a, 3) + (int)Math.pow(b, 3) + (int)Math.pow(c, 3));
-		if(a != b && b != c && c != a) {
-			answer = cal1;
-		}
-		if((a == b && b != c) || (b == c && c != a) || (c == a && a != b)) {
-			answer = cal1 * cal2;
-		}
-		if(a == b && b == c && c == a) {
-			answer = cal1 * cal2 * cal3;
-		}
-		return answer;
-	}
 }
